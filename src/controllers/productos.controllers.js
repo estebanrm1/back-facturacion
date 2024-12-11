e487bad33288a3d155bbe10e263a113d2ab7c285
Esteban Molina <solsel90@gmail.com>
2024-12-11 17:37:23 -0300

    Se creo la logica de la funcioncion borrar producto la cual borra un producto por su id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,4 @@
+import { log } from 'console';
 import Producto from '../models/producto';
 
 export const obtenerProductos = async (req, res) => {
@@ -38,3 +39,17 @@ export const crearProducto = async (req, res) => {
         });
     }
 };
+
+export const borrarProducto = async (req, res)=>{
+    try {
+        await Producto.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            mensaje: 'El producto fue eliminado conrrectamente'
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: 'Error no se pudo eliminar el producto',
+        });
+    }
+}
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { obtenerProductos, crearProducto, obtenerProducto } from "../controllers/productos.controllers";
+import { obtenerProductos, crearProducto, obtenerProducto, borrarProducto } from "../controllers/productos.controllers";
 
 const router = Router();
 
 
 
 router.route('/productos').get(obtenerProductos).post(crearProducto);
-router.route('/productos/:id').get(obtenerProducto);
+router.route('/productos/:id').get(obtenerProducto).delete(borrarProducto);
 
 
 export default router;
