4cf36c7a5eaad1372fe6fa0f46f805a37af39c11
Esteban Molina <solsel90@gmail.com>
2024-12-11 17:22:54 -0300

    Se creo la logica de la funcion obtener produto mediante el id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -12,6 +12,17 @@ export const obtenerProductos = async (req, res) => {
         });
     }
 };
+export const obtenerProducto = async (req, res) => {
+    try {
+        const producto = await Producto.findById(req.params.id);
+        res.status(200).json(producto);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: 'Error no se encontro el producto',
+        });
+    }
+};
 
 export const crearProducto = async (req, res) => {
     try {
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { obtenerProductos, crearProducto } from "../controllers/productos.controllers";
+import { obtenerProductos, crearProducto, obtenerProducto } from "../controllers/productos.controllers";
 
 const router = Router();
 
 
 
-router.route('/productos').get(obtenerProductos).post(crearProducto)
+router.route('/productos').get(obtenerProductos).post(crearProducto);
+router.route('/productos/:id').get(obtenerProducto);
+
 
 export default router;
